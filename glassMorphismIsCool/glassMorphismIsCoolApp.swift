2c28e4bf803fcb8823b846776e65644969183c4e
Nova <93756895+NovaSwiftDeveloper@users.noreply.github.com>
2022-08-18 20:39:27 +0300

    Initial Commit

---
+++ glassMorphismIsCool/glassMorphismIsCoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  glassMorphismIsCoolApp.swift
+//  glassMorphismIsCool
+//
+//  Created by Ravil Khusnullin on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct glassMorphismIsCoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
